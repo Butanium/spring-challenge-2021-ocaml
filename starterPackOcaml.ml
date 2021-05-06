fbb676bb0867d702c47dc43361dae6cc9685c526
Clement Dumas <dumasclement2002@gmail.com>
2021-05-06 23:18:17 +0200

    Added comment to the starter pack

--- starterPackOcaml.ml
+++ starterPackOcaml.ml
@@ -1,7 +1,20 @@
 (* Auto-generated code below aims at helping you parse *)
 (* the standard input according to the problem statement. *)
+
+
+(* to create a tree :
+let myTree = {pos=2; size=2; ismine = true; isdormant = false} *)
+(* you can access to any of the tree attribute with :
+let myTreePos = myTree.pos *)
 type tree = {pos : int; size : int; ismine : bool; isdormant : bool};;
+
+(* you can create an action like this :
+let a = GROW 2 and a2 = COMPLETE 3 and a3 = WAIT or
+use the action function below *)
 type action = GROW of int | COMPLETE of int | WAIT;;
+
+(* Convert an action string to an action type :
+let a = action "COMPLETE 3" return COMPLETE 3 *)
 let action s = if s.[0] = 'W' then WAIT else (
     let act, t = Scanf.sscanf s "%s %d" (fun a b -> a,b) in
     match act.[0] with
@@ -10,14 +23,23 @@ let action s = if s.[0] = 'W' then WAIT else (
     | _ -> failwith "not implemented"
     )
 
-and toString = function
+(* Convert an action to a string
+toString (COMPLETE 3) return "COMPLETE 3" *)
+and toString = function (* a good exemple on how you can
+match actions *)
     | GROW t -> Printf.sprintf "GROW %d" t
     | COMPLETE t -> Printf.sprintf "COMPLETE %d" t
     | WAIT -> "WAIT" in
 
+ (* create a dictionnary which you'll use when you reach bronze league*)
 let neighbourTable = Hashtbl.create 37
+
+(* create a dictionnary which will associate each cell to
+its richness *)
 and richnessTable = Hashtbl.create 37 in
+(* return the neighbour array (not used until bronze league) *)
 let getNeighbours x = Hashtbl.find neighbourTable x
+(* return the richness of the tile *)
 and richness x = Hashtbl.find richnessTable x  in
 
 let numberofcells = int_of_string (input_line stdin) in (* 37 *)
@@ -25,14 +47,16 @@ for i = 0 to numberofcells - 1 do
     (* index: 0 is the center cell, the next cells spiral outwards *)
     (* richness: 0 if the cell is unusable, 1-3 for usable cells *)
     (* neigh0: the index of the neighbouring cell for each direction *)
-    let index, richness, neigh0, neigh1, neigh2, neigh3, neigh4, neigh5 = Scanf.sscanf (input_line stdin) " %d  %d  %d  %d  %d  %d  %d  %d" (fun index richness neigh0 neigh1 neigh2 neigh3 neigh4 neigh5 -> (index, richness, neigh0, neigh1, neigh2, neigh3, neigh4, neigh5)) in
+    let index, richness, neighbours = Scanf.sscanf (input_line stdin) " %d  %d  %d  %d  %d  %d  %d  %d" (fun index richness neigh0 neigh1 neigh2 neigh3 neigh4 neigh5 -> (index, richness, [|neigh0; neigh1; neigh2; neigh3; neigh4; neigh5|])) in
+    (* fill the dictionnaries *)
     Hashtbl.add richnessTable index richness ;
-    Hashtbl.add neighbourTable index [neigh0; neigh1; neigh2; neigh3; neigh4; neigh5];
+    Hashtbl.add neighbourTable index neighbours;
 done;
 
 
 (* game loop *)
 while true do
+	(* create a table to store all the trees with their position as index*)
     let treeTable = Hashtbl.create 37 in
     let day = int_of_string (input_line stdin) in (* the game lasts 24 days: 0-23 *)
     let nutrients = int_of_string (input_line stdin) in (* the base score you gain from the next COMPLETE action *)
@@ -50,11 +74,13 @@ while true do
         (* ismine: 1 if this is your tree *)
         (* isdormant: 1 if this tree is dormant *)
         let tree = Scanf.sscanf (input_line stdin) " %d  %d  %d  %d" (fun cellindex size ismine isdormant -> {pos=cellindex; size=size; ismine = (ismine = 1); isdormant = (isdormant = 1)}) in
+        (* add the tree to the dictionnary *)
         Hashtbl.add treeTable tree.pos tree;
     done;
     let numberofpossiblemoves = int_of_string (input_line stdin) in
+    (* make a list of all the actions, replace the for loop in the codingame code *)
     let actionlist =
-    let rec aux n = if n=numberofpossiblemoves then [] else (action (input_line stdin) :: aux (n+1))
+    let rec aux n = if n = numberofpossiblemoves then [] else (action (input_line stdin) :: aux (n+1))
     in aux 0 in
 
 
