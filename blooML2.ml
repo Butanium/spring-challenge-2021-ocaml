7ba78dd71b3761a531375aed79fcae54421684fb
Clement Dumas <dumasclement2002@gmail.com>
2021-05-13 13:07:24 +0200

    say hello to blooML 2.0 !

--- blooML2.ml
+++ blooML2.ml
@@ -1,6 +1,6 @@
 
-(* TODO (1) optimize plants at the end, we need less seeds and more trees
- adjust the optimization function *)
+(* TODO adjust endGame, we could optimize it *)
+
 
 let f = float_of_int;;
 module IntSet = Set.Make(Int);;
@@ -115,21 +115,61 @@ let getPotentialAllyShadow tree =
     in -. exp (loop 0 0. /. 2.) /. 5. +. 1. /. 5. in
 
 let getSelfShadowSize tree treeSize =
-    let rec aux (pos:int) dir depth acc =
+    let rec aux (pos:int) dir depth ((maxHeight,hPos),acc) =
         let n =  (getNeighbours pos).(dir) in
         if n = -1 || depth > treeSize then acc else (
             aux n dir (depth + 1) @@
             match getTreeOpt n with
-            (* if t.size < depth sen less malus *)
-            | Some t when t.ismine && t.size <= treeSize -> acc + t.size
-            | Some t when not t.ismine && t.size < treeSize -> acc - t.size
-            | _ -> acc
+            (* if t.size < depth then less malus *)
+            | Some t -> (if t.size >= maxHeight then t.size, depth else maxHeight, hPos),
+                if t.size > maxHeight || depth - hPos > maxHeight then acc + (if t.ismine then t.size else -t.size)
+                else acc
+            | _ -> (maxHeight,hPos),acc
         )
     and loop i acc = if i = 6 then acc else
-        loop (i+1) (acc + aux tree i 1 0)
+        loop (i+1) (acc + aux tree i 1 ((0,0),0))
     in f @@ loop 0 0 in
 let getSelfShadow tree =
     getSelfShadowSize tree (match getTreeOpt tree with | None -> -1 | Some t -> t.size)  in
+
+let getNextDir day = (day + 1) mod 6 and
+oppDir dir = (dir + 3) mod 6 in
+let rec origin pos dir =
+    match (getNeighbours pos).(oppDir dir) with
+        | -1 -> pos
+        | p -> origin p dir
+in
+
+let getTreeRowIncomes tree dir =
+    let shadows = Array.make 7 0 in
+    let addShadows depth size =
+        for k = depth+1 to min (depth+size) 6 do
+            shadows.(k) <- max shadows.(k) size
+        done
+    in
+    let incomes depth tree = addShadows depth tree.size;
+        (if tree.ismine then 1 else -1) * if tree.size > shadows.(depth) then tree.size else 0
+    in
+    let rec aux pos depth acc = if pos = -1 then acc else
+        aux (getNeighbours pos).(dir) (depth + 1) @@
+        if pos = tree.pos then acc + incomes depth tree
+        else match getTreeOpt pos with
+               | Some t -> acc + incomes depth t
+               | _ -> acc
+    in aux (origin tree.pos dir) 0 0
+in
+
+let getDiff treeInit newTree turns day =
+    let newI = getTreeRowIncomes newTree and
+    oldI = getTreeRowIncomes treeInit in
+    let diff i = newI (getNextDir @@ day + i) - oldI (getNextDir @@ day + i) in
+    let rec loop i acc = if i = turns then acc else
+        loop (i + 1) (acc + diff i)
+    in loop 0 0
+in
+let getDiffF treeInit newTree turns day =
+    f (getDiff treeInit newTree turns day)
+in
 
 (* game loop *)
 while true do
@@ -140,12 +180,9 @@ while true do
     prerr_endline @@ dayDebug;
     let dayf = f day in
     let nutrients = int_of_string (input_line stdin) in (* the base score you gain from the next COMPLETE action *)
-    (* sun: your sun points *)
-    (* score: your current score *)
+
     let sun, score = Scanf.sscanf (input_line stdin) " %d  %d" (fun sun score -> (sun, score)) in
-    (* oppsun: opponent's sun points *)
-    (* oppscore: opponent's score *)
-    (* oppiswaiting: whether your opponent is asleep until the next day *)
+
     let oppsun, oppscore, oppiswaiting = Scanf.sscanf (input_line stdin) " %d  %d  %d" (fun oppsun oppscore oppiswaiting -> (oppsun, oppscore, oppiswaiting = 1)) in
     let numberoftrees = int_of_string (input_line stdin) and (* the current amount of trees *)
     sizeCount = Array.make 4 0
@@ -178,21 +215,21 @@ while true do
     let getScore a = let s1,s2 = (
         match a with
         | COMPLETE t -> let nb3 = sizeCount.(3) in -. 4. +. 20. ** ((dayf/. maxDayf)**2.) +.
-            (if nb3>2 then f nb3 else 0.)+. (if day=maxDay then 10. else( -. getPotentialAllyShadow t +. getSelfShadow t -. 2.) +. (min 0. (4. *.(1. -. enTreeRatio)))), f (richBonus t)
+            (if nb3>2 then f nb3 else 0.)+. (if day=maxDay then 10. else (min 0. (4. *.(1. -. enTreeRatio))))
+            +. 2. +.  getDiffF (getTree t) (makeSeed t) 2 (min 2 (maxDay-day)) +. max 0. (f sun -. 5.),
+            f (richBonus t)
         | SEED (x,y) -> if maxDay - day < 3 then -1. , 0. else (if day <= 1 then 0. else 10.) *. (exp (-.2.4 *.(f sizeCount.(0))**2.))
         , 0.25 *. f(richBonus y) +. 2. *.getPotentialAllyShadow y
-        | GROW t -> let size = (getTree t).size in if maxDay - day + size < 3 then -1. , 0. else 5. +. 2. *.(f size -. 1.5)*.dayf/.maxDayf -. f sizeCount.(size+1)
-         -. getSelfShadowSize t (size+1) +. getSelfShadowSize t size, f (richBonus t)
+        | GROW t ->let tree = getTree t in let size = tree.size in if maxDay - day + size < 3 then -1. , 0. else 5. +. 2. *.(f size -. 1.5)*.dayf/.maxDayf -. f sizeCount.(size+1)
+         +. 2. +. getDiffF tree ({pos=t; size=size + 1; ismine=true; isdormant=true}) (min 2 (maxDay-day)) day , f (richBonus t)
         | WAIT -> 0., 0.
 
         ) in s1 *. (if (match a with | SEED _ -> true | _ -> false) || possible a then 1. else 0.5),
          if (match a with SEED _ -> true | _ ->false) && not @@ possible a then 0.5 *. s2 else s2
     in
 
-
     let myActions = getActionTrees myTrees in
     assert (List.fold_left (fun acc x -> acc + if possible x then 1 else 0) 0 myActions = numberofpossiblemoves);
-
 
     let sortedAction = List.sort (fun x y ->
         let (xs1, xs2), (ys1, ys2) = (getScore x), (getScore y) in -2*compare xs1 ys1 - compare xs2 ys2) myActions in
---
+++ blooML2.mli
@@ -0,0 +1,36 @@
+val isPossible : int -> int array -> action -> bool
+(** [isPossible sun sizeArrays action] returns true if the action can be done this turn *)
+
+val getSelfShadow : int -> float
+(** [getSelfShadow t] returns +tree.size per ally tree hidden by t and
+-tree.size per enemy tree strictly hidden by t*)
+
+val getSelfShadowSize : int -> int -> float
+(** [getSelfShadowSize p s] run getSelfShadow as if there where a tree of size s at the position p *)
+
+val toString : action -> string
+(** [toString a] returns the string representation of the action *)
+
+val getNearTiles : int -> int -> int list
+(** [getNearTiles t depth] returns all the tiles at the range depth of the t position *)
+
+val getActionTrees : tree list -> action list
+(** [getActionTrees trees] returns all the action that can be done by trees (doesn't care about
+the tree being dormant or not and about the cost of the action) *)
+
+val getPrice : int array -> action -> int
+(**[getPrice sizeCount a] returns the amount of sun needed to perform the action a *)
+
+val getTarget : action -> int
+(** [getTarget a] returns the tree that need to be awaken for the action to be performed *)
+
+val richBonus : int -> int
+(** [richBonus t] returns the richness bonus that will provide this cell *)
+
+val getScore : action -> float
+(** [getScore a] returns 2 score : the first one is the valueability of the action comparing to other action
+and the second is a secondary filter to choose between 2 action with the same primary score *)
+
+val getDiff : tree -> tree -> int -> int -> int
+(** [getDiff treeInit newTree turns day] returns the difference of incomes for the turns next turns if
+treeInit is replaced per newTree*)
